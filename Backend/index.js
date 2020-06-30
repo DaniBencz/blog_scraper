16b1142e2b4dd2ef89782d66b3ad6bfa17e543eb
Daniel <daniel.bencz@gmail.com>
2020-06-30 18:34:21 +0200

    first build

---
+++ Backend/index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+// api to listen to FE requests
+app.get('/articles', (req, res) => {
+	res.json({
+		"hello": "world"
+	});
+});
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
---
+++ Backend/public/precache-manifest.e575d6123868c3993b3f6e672fd087f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fee669cfd2504525f1dbcfd3a3926ff7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13b327a70581b471d73d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "918ace7e5dd6626320f0",
+    "url": "/static/js/2.737efa16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.737efa16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13b327a70581b471d73d",
+    "url": "/static/js/main.e696eeab.chunk.js"
+  },
+  {
+    "revision": "ae7cb4dbfbdb64cdb782",
+    "url": "/static/js/runtime-main.3a3fa9f1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
