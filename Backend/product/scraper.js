a4de72b3b25819960c06d8450a0eac92f84d46cd
Daniel <daniel.bencz@gmail.com>
2020-07-04 16:58:23 +0200

    preparing for DB

--- Backend/product/scraper.js
+++ Backend/product/scraper.js
@@ -3,7 +3,7 @@
 const request = require('request-promise');
 const cheerio = require('cheerio');
 
-const scrapeArticlesByPage = (articles) => {
+const scrapeArticlesByPage = (articles, useDB) => {
 	return new Promise((resolve, reject) => {
 		let articles_without_image = [];
 		let article_index = 0;
@@ -31,7 +31,7 @@ const scrapeArticlesByPage = (articles) => {
 			}
 			else {
 				console.log('done scraping');
-				// maybe no need for promise?
+				if (useDB) saveArticlesInDB(articles_without_image);
 				resolve(articles_without_image);
 			}
 		}
@@ -39,7 +39,7 @@ const scrapeArticlesByPage = (articles) => {
 	});
 }
 
-const getArticlesByPage = (page_num = false) => {
+const filterArticlesByPage = (page_num, useDB) => {
 	return new Promise((resolve, reject) => {
 		const path = page_num > 1 ? '/page/' + page_num : '';
 		const options = {
@@ -54,31 +54,44 @@ const getArticlesByPage = (page_num = false) => {
 				$(".main-inner article .post-title a").each((i, el) => {
 					articles.push($(el).attr("href"));
 				});
-				resolve(scrapeArticlesByPage(articles));
+				resolve(scrapeArticlesByPage(articles, useDB));
 			})
 			.catch(err => {
 				console.log('request error: ', err);
 				reject('request error');
 			});
+	})
+}
+
+const getArticlesByPage = (page_num, useDB) => {
+	return new Promise((resolve, reject) => {
+
+		//if useDB
+			// lookup data in db
+				// if found
+					// resolve data
+				// else resolve(filterArticlesByPage(page_num));
+		//else resolve(filterArticlesByPage(page_num));
+
+		resolve(filterArticlesByPage(page_num, useDB));
 	});
 }
 
-const getAllArticles = (pages = 1) => {
+const getAllArticles = (pages = 1, useDB = false) => {
 	return new Promise((resolve, reject) => {
-		let res = []
+		let articles_per_page_promises = [];
 		for (let i = 1; i <= pages; i++) {
-			res.push(getArticlesByPage(i))
+			articles_per_page_promises.push(getArticlesByPage(i, useDB));
 		}
-		Promise.all(res)
+		Promise.all(articles_per_page_promises)
 			.then((articles_per_page) => {
 				let all_articles = articles_per_page.reduce((acc, cur) => {
-					return acc.concat(cur)
-				}, [])
+					return acc.concat(cur);
+				});
 				resolve(all_articles);
 			})
 			.catch(err => console.log(err))
 	})
-
 }
 
 getAllArticles().then(list => console.log(list));
