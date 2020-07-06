3d35ac958f6a42e549d998473fc6b049e6e9626f
Daniel <daniel.bencz@gmail.com>
2020-07-06 16:11:42 +0200

    first input-based render

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,31 +1,39 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import List from './components/List';
 import axios from 'axios';
 
 function App() {
+	let pages = useRef(0);
+	const [articles, setArticles] = useState([]);
+
+	const updatePages = (event) => {
+		pages = Number(event.target.value);
+	}
+
 	const getArticles = () => {
 		axios({
 			//url: `${window.location.origin}/articles`,
 			url: 'http://localhost:4000/articles',
 			method: 'post',
 			data: {
-				pages: 2
+				pages: pages
 			}
 		})
 			.then(resp => {
-				resp.data.articles.forEach(element => {
-					console.log(element)
-				});
+				setArticles(resp.data.articles)
+			})
+			.catch(resp => {
+				//
 			})
 	}
 
 	return (
 		<div>
-			<input id="page" type="text" placeholder="type a number between 1 and 5"></input>
+			<input id="page" type="text" placeholder="type a number between 1 and 5" onChange={updatePages}></input>
 			<button id="get_articles" onClick={getArticles}>Get Articles</button>
-			<List articles={['foo', 'bar', 'baz']}></List>
+			<List articles={articles}></List>
 		</div>
 	);
 }
